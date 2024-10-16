8928d5b86cb5a85f2a420384ae690232f46717b8
emanuela <emanuela.markovic@hotmail.com>
2024-10-16 16:20:15 +0200

    Update Tailwind CSS configuration with custom colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,25 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: "#000000",
+        light: "#f5f5f5",
+        primary: "#B63E96",
+        primaryDark: "#58E6D9",
+      },
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+        montserrat: ["Montserrat", ...fontFamily.sans],
       },
     },
   },
